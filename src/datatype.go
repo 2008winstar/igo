b2f6d2a014d9f626328bd2da4b66e4e28ae039ef
2008winstar <2008winstar@sina.com>
2019-02-02 16:24:06 +0800

    add demo

--- src/datatype.go
+++ src/datatype.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 basic data types:
 Integers:
--- src/marshalJson.go
+++ src/marshalJson.go
@@ -19,4 +19,31 @@ func main() {
 	}
 
 	fmt.Println(string(byteArray))
+
+	type Author struct {
+		Sales     int  `json:"book_sales"`
+		Age       int  `json:"age"`
+		Developer bool `json:"is_developer"`
+	}
+
+	type Book1 struct {
+		Title  string `json:"title"`
+		Author Author `json:"author"`
+	}
+
+	author := Author{Sales: 3, Age: 25, Developer: true}
+	book1 := Book1{Title: "Learning Concurrency in Python", Author: author}
+
+	byteArray, err = json.Marshal(book1)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(byteArray))
+
+	byteArray, err = json.MarshalIndent(book1, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(byteArray))
 }
